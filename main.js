d225485a89ccabb45baacc75dc2aabe9fb374618
proshu <72331686+proshu@users.noreply.github.com>
2021-05-11 19:40:24 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ window.onload = function() {
   const spinner = document.getElementById('loading');
   spinner.classList.add('loaded');
 }
+window.addEventListener("load",function() {
+  setTimeout(loaded, 500)
+})
+
+
+setTimeout(loaded, 5000)
